ececbd8e7b64bdb6552c9eb58b1747a071450163
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-07-19 20:55:42 +0600

    place details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllPlaces from './pages/AllPlaces';
+import PlaceDetails from './pages/PlaceDetails';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/rooms' element={<AllPlaces/>}/>
+          <Route path='/rooms/:id' element={<PlaceDetails/>}/>
         </Routes>
       </div>
       <Footer></Footer>
