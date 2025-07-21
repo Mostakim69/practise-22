3930129100338caefd9accec9ced26e718fbf1b6
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-07-22 04:15:33 +0600

    Hero Section profesional

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@ import { assets, cities } from '../assets/assets';
 
 const Hero = () => {
     return (
-        <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
+        <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/bannerImage01.jpg")] bg-no-repeat bg-cover bg-center h-screen'>
             <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20'>The Ultimate Travel Experience</p>
-            <h1 className='font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4'>Discover Your Perfect Gateway Destination</h1>
-            <p className='max-w-130 mt-2 text-sm md:text-base'>This is a travel .....</p>
+            <h1 className='font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4'>Journey Through the Heart of Bangladesh</h1>
+            <p className='max-w-130 mt-2 text-lg md:text-base'>Discover hidden gems, vibrant culture & unforgettable adventures with Tour Nest BD.</p>
             <form className='bg-white text-gray-500 rounded-lg mt-8 px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
 
                 <div>
